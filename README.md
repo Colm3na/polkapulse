29999c7cd73aac54d1bb4c20a38acdcf6de81887
mariopino <mariopino@protonmail.com>
2021-08-02 11:48:40 +0200

    decentralized version

--- README.md
+++ README.md
@@ -15,8 +15,6 @@ We are using Kusama instead Polkadot cause Polkadot is currently in PoA mode so 
 The circle outside shadow/blur is dinamic and depends on the number of events included in the last block. That way you can visualize the network
 activity with a nice heartbeat.
 
-Polka Pulse was built using [PolkaStats Backend V3](https://github.com/Colm3na/polkastats-backend-v3) graphql API to easily fetch the live chain data.
-
 ### Try it out
 
 You'll need a PolkaStats Backend V3 API instance already running.
@@ -27,23 +25,6 @@ Clone the repo:
 git clone https://github.com/Colm3na/polkapulse.git
 cd polkapulse
 yarn
-```
-
-Edit `app/main.js`, in line 4 and 19 enter the full PolkaStats Backend V3 API http and websocket URLS:
-
-```
-const httpLink = new HttpLink({
-  // You should use an absolute URL here
-  uri: 'POLKASTATS_BACKEND_HTTP_GRAPHQL_ENDPOINT',
-})
-
-// Create the subscription websocket link
-const wsLink = new WebSocketLink({
-  uri: 'POLKASTATS_BACKEND_WS_GRAPHQL_ENDPOINT',
-  options: {
-    reconnect: true,
-  },
-})
 ```
 
 Then run development server:
--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import { ApolloClient } from 'apollo-client'
-// import { HttpLink } from 'apollo-link-http'
-// import { InMemoryCache } from 'apollo-cache-inmemory'
-// import { split } from 'apollo-link'
-// import { WebSocketLink } from 'apollo-link-ws'
-// import { getMainDefinition } from 'apollo-utilities'
-// import VueApollo from 'vue-apollo'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import VueGtag from "vue-gtag";
 
@@ -16,47 +9,6 @@ Vue.use(VueGtag, {
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
-// const httpLink = new HttpLink({
-//   // You should use an absolute URL here
-//   uri: 'https://kusama.polkastats.io/api/v3',
-// })
-
-// // Create the subscription websocket link
-// const wsLink = new WebSocketLink({
-//   uri: 'wss://kusama.polkastats.io/api/v3',
-//   options: {
-//     reconnect: true,
-//   },
-// })
-
-// // using the ability to split links, you can send data to each link
-// // depending on what kind of operation is being sent
-// const link = split(
-//   // split based on operation type
-//   ({ query }) => {
-//     const definition = getMainDefinition(query)
-//     return definition.kind === 'OperationDefinition' &&
-//       definition.operation === 'subscription'
-//   },
-//   wsLink,
-//   httpLink
-// )
-
-// // Create the apollo client
-// const apolloClient = new ApolloClient({
-//   link,
-//   cache: new InMemoryCache(),
-//   connectToDevTools: true,
-// })
-
-// const apolloProvider = new VueApollo({
-//   defaultClient: apolloClient,
-// })
-
-// // Install the vue plugin like before
-// Vue.use(VueApollo)
-
 new Vue({
-  // apolloProvider: apolloProvider,
   render: h => h(App)
 }).$mount('#app')
